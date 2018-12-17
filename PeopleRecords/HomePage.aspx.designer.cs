a99989c0fc45eb772ff7805c8165cb37f7d87831
saketh1993 <k.s.saketh@gmail.com>
2018-12-16 21:35:50 -0600

    changes to designer

--- PeopleRecords/HomePage.aspx.designer.cs
+++ PeopleRecords/HomePage.aspx.designer.cs
@@ -47,5 +47,23 @@ namespace PeopleRecords {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlInputText txtLastName;
+        
+        /// <summary>
+        /// btnCancel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCancel;
+        
+        /// <summary>
+        /// btnSubmit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSubmit;
     }
 }
