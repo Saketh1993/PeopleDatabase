f14c73a575a804b1c4ee3ea5464a7746f94ca589
saketh1993 <saketh1993@faafa34d-97e0-cb2b-7c60-ce57b7db7bff>
2018-12-19 02:25:08 +0000

    added newton json dll and DB class
    git-svn-id: https://github.com/Saketh1993/PeopleDatabase.git/trunk@10 faafa34d-97e0-cb2b-7c60-ce57b7db7bff

---
+++ PeopleRecords/Models/DB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace PeopleRecords.Models
+{
+    public class DB
+    {
+        protected Int32 GetStates(out DataTable dtbl)
+        {
+            using (var conn = new SqlConnection(WebConfigurationManager.AppSettings["ConnectionString"]))
+            {
+                dtbl = new DataTable("uspStateList");
+                SqlCommand cmd = new SqlCommand("", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandTimeout = 500;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dtbl);
+                return (Int32)cmd.Parameters["Return_Value"].Value;
+            }
+        }
+    }
+}
